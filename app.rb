493e4bbc1ced8270d540bf4a7886811ffe4df315
jovi <kwong.jovi@gmail.com>
2022-09-12 15:04:55 +0100

    phase2 bite1 challenge1 - completed(2)
    Co-authored-by: Leroy <leroy_jallow_@hotmail.co.uk>

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Application < Sinatra::Base
-  # This allows the app code to refresh
-  # without having to restart the server.
+  
   configure :development do
     register Sinatra::Reloader
   end
