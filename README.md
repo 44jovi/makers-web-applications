cd3ab06f799d7ef0ade453fbc1f4bed12d6d46aa
jovi <kwong.jovi@gmail.com>
2022-09-13 09:14:26 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - This repo is my collection of project work for the Web Applications module of the Makers Academy Bootcamp
   - https://github.com/makersacademy/web-applications/
+- Collaborators and co-authors are from pair programming sessions
--- phase2/bite4/music_library_database_app-bite4/spec/integration/application_spec.rb
+++ phase2/bite4/music_library_database_app-bite4/spec/integration/application_spec.rb
@@ -10,4 +10,39 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context 'GET /albums' do
+    it 'returns all albums' do
+      response = get('/albums')
+
+      result = 'Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(result)      
+    end
+  end
+
+  context "POST /albums" do
+    it 'creates a new album' do
+      response = post(
+        '/albums',
+        title: 'Bleach',
+        release_year: '1989',
+        artist_id: '44'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('Album created!')
+
+      # check to see if it appears on new list of albums:
+      response = get('/albums')
+      expect(response.body).to include('Bleach')
+    end
+
+    it 'returns 404 Not Found' do
+      response = post('/albumsqwerty')
+
+      expect(response.status).to eq(404)
+    end
+  end
+
 end
