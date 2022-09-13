6f96bfdc3e233921c7dddfc8d63f55036f7f9c10
jovi <kwong.jovi@gmail.com>
2022-09-13 13:44:22 +0100

    phase3 bite2 exercise1 - completed

---
+++ phase3/music_library_database_app-phase3-bite2/app.rb
@@ -0,0 +1,87 @@
+# file: app.rb
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
+
+DatabaseConnection.connect
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/album_repository'
+    also_reload 'lib/artist_repository'
+  end
+
+  get '/albums' do
+    repo = AlbumRepository.new
+    albums = repo.all
+
+    response = albums.map do |album|
+      album.title
+    end.join(', ')
+  end
+
+  # see end of this file for my original inefficient solution
+  get '/albums/:id' do
+    album_repo = AlbumRepository.new
+    @album = album_repo.find(params[:id])
+
+    artist_repo = ArtistRepository.new
+    @artist = artist_repo.find(@album.artist_id) 
+
+    return erb(:album)
+  end
+
+  post '/albums' do
+    album = Album.new         
+    album.title = params[:title]
+    album.release_year = params[:release_year]
+    album.artist_id = params[:artist_id]
+
+    repo = AlbumRepository.new
+    repo.create(album)
+
+    return "Album created!"
+  end
+
+  get '/artists' do
+    repo = ArtistRepository.new
+    artists = repo.all
+
+    response = artists.map do |artist|
+      artist.name
+    end.join(', ')
+  end
+
+  post '/artists' do
+    artist = Artist.new         
+    artist.name = params[:name]
+    artist.genre = params[:genre]
+
+    repo = ArtistRepository.new
+    repo.create(artist)
+
+    return "" # this time return nothing, unlike when POST /albums
+  end
+
+
+
+end
+
+# # my original solution for get '/albums/:id'
+# get '/albums/:id' do
+#   repo = AlbumRepository.new
+#   album = repo.find(params[:id])
+
+#   @album_title = album.title
+#   @album_release_year = album.release_year
+#   @album_artist_id = album.artist_id
+
+#   repo = ArtistRepository.new
+#   artist = repo.find(@album_artist_id)
+#   @artist_name = artist.name
+
+#   return erb(:index)
+# end
---
+++ phase3/music_library_database_app-phase3-bite2/spec/integration/application_spec.rb
@@ -0,0 +1,134 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+def reset_albums_table
+  seed_sql = File.read('spec/seeds/albums_seeds.sql')
+  user = ENV['PGUSER1']
+  password = ENV['PGPASSWORD']
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test', user: user, password: password })
+  connection.exec(seed_sql)
+end
+
+def reset_artists_table
+  seed_sql = File.read('spec/seeds/artists_seeds.sql')
+  user = ENV['PGUSER1']
+  password = ENV['PGPASSWORD']
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test', user: user, password: password })
+  connection.exec(seed_sql)
+end
+
+describe Application do
+  # reset tables before each test
+  before(:each) do 
+    reset_albums_table    
+    reset_artists_table
+  end
+
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context 'GET /albums' do
+    it 'returns all albums' do
+      response = get('/albums')
+
+      expected_response = 'Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)      
+    end
+
+    it 'returns 404 Not Found' do
+      response = post('/qwerty123456')
+      expect(response.status).to eq(404)
+    end
+  end
+
+  context "GET /albums/:id" do
+    it "returns details of a specified album ('Doolittle')" do
+      response = get("/albums/1")
+      
+      expect(response.status).to eq(200)
+
+      expect(response.body).to include("<h1>Doolittle</h1>")
+      expect(response.body).to include("Release year: 1989")
+      expect(response.body).to include("Artist: Pixies")
+    end
+
+    it "returns details of a specified album ('Surfa Rosa')" do
+      response = get("/albums/2")
+      
+      expect(response.status).to eq(200)
+
+      expect(response.body).to include("<h1>Surfer Rosa</h1>")
+      expect(response.body).to include("Release year: 1988")
+      expect(response.body).to include("Artist: Pixies")
+    end
+  end
+
+  context "POST /albums" do
+    it 'creates a new album' do
+      response = post(
+        '/albums',
+        title: 'Bleach',
+        release_year: '1989',
+        artist_id: '44'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('Album created!')
+
+      # check to see if it appears on new list of albums:
+      response = get('/albums')
+      expect(response.body).to include('Bleach')
+    end
+
+    it 'returns 404 Not Found' do
+      response = post('/qwerty123456')
+      expect(response.status).to eq(404)
+    end
+  end  
+
+  context "GET /artists" do
+    it 'returns 200 OK and list of artists' do
+      response = get('/artists')
+      expected_response = "Pixies, ABBA, Taylor Swift, Nina Simone"
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+
+    it 'returns 404 Not Found' do
+      response = get('/qwerty123456')
+
+      expect(response.status).to eq(404)
+    end
+  end
+
+  context "POST /artists" do
+    it 'returns 200 OK and creates an artist' do
+      response = post(
+        '/artists',
+        name: 'Wild nothing',
+        genre: 'Indie'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get('/artists')
+      expect(response.body).to include('Wild nothing')
+    end
+
+    it 'returns 404 Not Found' do
+      response = post('/qwerty123456')
+
+      expect(response.status).to eq(404)
+    end
+  end
+
+end
